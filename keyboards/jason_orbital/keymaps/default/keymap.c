108503ca3ca9fbf8c870b82292fe8ac9eb8f1305
Seojoon Yeon <seojoony9000@gmail.com>
2025-11-22 12:33:15 -0600

    fully functional

--- keyboards/jason_orbital/keymaps/default/keymap.c
+++ keyboards/jason_orbital/keymaps/default/keymap.c
@@ -1,32 +1,40 @@
 #include QMK_KEYBOARD_H
 
+uint8_t encoder_counter = 0;
+
 enum layer_names {
     _BASE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_L, KC_U, KC_R,
-        KC_A, KC_D, KC_D,
+        KC_A, KC_A, KC_A,
+        KC_B, KC_B, KC_B,
 
-        KC_L, KC_U, KC_R,
-        KC_G, KC_D, KC_F
+        KC_C, KC_C, KC_C,
+        KC_D, KC_D, KC_D
     )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
+    encoder_counter++;
+
+    if (encoder_counter % 2 == 0) {
+        return false;
+    }
+    
     // if (is_keyboard_left()) {
     if (index == 0) {
         if (clockwise) {
-            tap_code16(KC_E);   // rotate right
+            tap_code16(KC_A);   // rotate right
         } else {
-            tap_code16(KC_F);   // rotate left
+            tap_code16(KC_B);   // rotate left
         }
     } else {
         if (clockwise) {
-            tap_code16(KC_G);   // move forward
+            tap_code16(KC_D);   // move forward
         } else {
-            tap_code16(KC_H);   // move backward
+            tap_code16(KC_C);   // move backward
         }
     }
     return false;
